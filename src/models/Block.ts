03ca141decf0a06c9465f1c3e4b399c51c3757e0
Lucas Neves <lucas54neves@gmail.com>
2024-06-15 12:13:11 -0300

    feat: add block doc

--- src/models/Block.ts
+++ src/models/Block.ts
@@ -1,12 +1,24 @@
+/**
+ * Represents a block in the blockchain.
+ */
 export class Block {
   index: number
   hash: string
 
+  /**
+   * Creates a new instance of the Block class.
+   * @param index - The index of the block.
+   * @param hash - The hash of the block.
+   */
   constructor(index: number, hash: string) {
     this.index = index
     this.hash = hash
   }
 
+  /**
+   * Checks if the block is valid.
+   * @returns True if the block is valid, false otherwise.
+   */
   isValid(): boolean {
     if (this.index < 0) {
       return false
